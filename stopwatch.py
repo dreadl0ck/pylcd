7c23bb97b844d30385571588059402cc5cc8878c
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-11-15 17:25:51 +0100

    fixes

--- stopwatch.py
+++ stopwatch.py
@@ -7,9 +7,12 @@ while True:
     try:
         time.sleep(1)
         seconds += 1
-        print(seconds)
+        fmt = time.strftime('%H:%M:%S', time.gmtime(seconds))
+        print(fmt)
+
         lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
-        lcd.lcd_string(str(seconds), 2)
+        lcd.lcd_string(fmt, 2)
+        
         #lcd.lcd_byte(lcd.LCD_LINE_2, lcd.LCD_CMD)
         #lcd.lcd_string("seconds", 2)
     except KeyboardInterrupt:
